9a2901f5ffda3ebd7b9f56f53c71cdb7593358fe
Chris Thorne <u1400900@umail.utah.edu>
2022-01-23 18:19:22 -0700

    template start

---
+++ src/Template.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const Manager = require('../lib/Manager');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+
